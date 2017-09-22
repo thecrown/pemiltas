5fd6b85d9ddf98ffc15aa7e8f9c803eef68b888b
thecrown <amienkurniawan5@gmail.com>
2017-09-22 08:45:45 +0700

    22 sept 2017

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,7 +49,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'welcome';
+$route['default_controller'] = 'login/user';
 // admin
 $route['dashboard'] = 'admin';
 $route['dashboard/login'] = 'login/admin';
